59516840fe496de9df838c94db2151aa5fd95227
thekelvinliu <kelvin@thekelvinliu.com>
2017-05-04 23:18:23 +0800

    trying to get ethjs to play nice -- a lot of Buffer issues

--- App/screens/HomeScreen.js
+++ App/screens/HomeScreen.js
@@ -1,7 +1,12 @@
+import Eth from 'ethjs';
+
 import React, { Component } from 'react';
 import { Button, Platform, StatusBar, Text, View } from 'react-native';
 import { Entypo } from '@expo/vector-icons';
 import IconButton from '../components/IconButton';
+
+const eth = new Eth(new Eth.HttpProvider('https://ropsten.infura.io'));
+// const acc = '0xeb61b66ea48a8834d4099a6276982cc211b0604d';
 
 const style = {
   marginTop: 100,
@@ -28,6 +33,31 @@ export default class extends Component {
       ),
     };
   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      blockNumber: '',
+      isLoading: true
+    };
+  }
+  componentDidMount() {
+    // this.getBalance(acc).done();
+    this.getBlockNumber().done();
+    // this.setState({ isLoading: false });
+  }
+  // not working yet
+  // async getBalance(account) {
+  //   this.setState({
+  //     balance: Eth.fromWei(await eth.getBalance(account), 'ether')
+  //   });
+  // }
+  async getBlockNumber() {
+    const bn = await eth.blockNumber();
+    this.setState({
+      blockNumber: bn.toString(),
+      isLoading: false
+    });
+  }
   render() {
     return (
       <View>
@@ -37,6 +67,11 @@ export default class extends Component {
           title="chat with alice"
           onPress={() => this.props.navigation.navigate('Chat', { username: 'alice' })}
         />
+        <Text style={style}>{
+          (this.state.isLoading)
+            ? 'loading block number'
+            : `the current block number is ${this.state.blockNumber}`
+        }</Text>
       </View>
     );
   }
