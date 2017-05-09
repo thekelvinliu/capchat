8d616e10aea220d1f8d22eb4c5983f24bef609be
thekelvinliu <kelvin@thekelvinliu.com>
2017-05-09 21:59:52 +0800

    changes for new wallet structure

--- App/ducks/wallet.js
+++ App/ducks/wallet.js
@@ -1,6 +1,3 @@
-import { Wallet } from 'ethers';
-import { REHYDRATE } from 'redux-persist/constants';
-
 import config from '../config';
 
 // index for the wallets in config
@@ -16,62 +13,31 @@ const issueWallet = () => {
   // return the wallet
   return retval;
 };
-const loadWallet = phrase => ((phrase) ? Wallet.fromMnemonic(phrase) : null);
 
 // actions
-const ADD_MNEMONIC = 'wallet/ADD_MNEMONIC';
-const ADD_WALLET = 'wallet/ADD_WALLET';
+const CREATE_WALLET = 'wallet/CREATE_WALLET';
 const REMOVE_WALLET = 'wallet/REMOVE_WALLET';
 
 // action creators
-export const addMnenomic = mnemonic => ({
-  type: ADD_MNEMONIC,
-  mnemonic
-});
-export const addWallet = instance => ({
-  type: ADD_WALLET,
-  instance
+export const createWallet = () => ({
+  type: CREATE_WALLET,
+  wallet: issueWallet()
 });
 export const removeWallet = () => ({
   type: REMOVE_WALLET
 });
-// thunks
-export const createWallet = () => async dispatch => {
-  // use wallets preloaded with ether instead of completely new wallets
-  // const wallet = await Wallet.createRandom({});
-  // issue a wallet
-  const { mnemonic } = issueWallet();
-  // load actual wallet instance
-  const wallet = await loadWallet(mnemonic);
-  // dispatch actions
-  dispatch(addMnenomic(wallet.mnemonic));
-  dispatch(addWallet(wallet));
-};
 
 // default to nulls
 const defaultState = {
-  instance: null,
-  mnemonic: null
+  address: null,
+  mnemonic: null,
+  privateKey: null
 };
 
 export default function(state = defaultState, action = {}) {
   switch (action.type) {
-    case ADD_MNEMONIC:
-      return {
-        ...state,
-        mnemonic: action.mnemonic
-      };
-    case ADD_WALLET:
-      return {
-        ...state,
-        instance: action.instance
-      };
-    case REHYDRATE:
-      const { mnemonic } = action.payload.wallet || state;
-      return {
-        instance: loadWallet(mnemonic),
-        mnemonic
-      };
+    case CREATE_WALLET:
+      return action.wallet;
     case REMOVE_WALLET:
       return defaultState;
     default:
